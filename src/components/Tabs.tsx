d5bda45c0c4de81da701d27da595af2469b38017
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-08-02 18:50:00 +0300

    add new task solution

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,34 +1,47 @@
 import React from 'react';
 import cn from 'classnames';
 import { Tab } from '../types/Tab';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 type Props = {
   tabs: Tab[];
+  activeTabId?: string;
+  onTabSelected?: (id: string) => void;
 };
 
-export const Tabs: React.FC<Props> = ({ tabs }) => {
-  const { tabId } = useParams<{ tabId?: string }>();
-  const activeTab = tabs.find(tab => tab.id === tabId);
+export const Tabs: React.FC<Props> = ({ tabs, activeTabId, onTabSelected }) => {
+  const currentActiveTab = tabs.find(tab => tab.id === activeTabId);
 
   return (
     <>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              data-cy="Tab"
-              className={cn({ 'is-active': tab.id === tabId })}
-              key={tab.id}
-            >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-            </li>
-          ))}
-        </ul>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                data-cy="Tab"
+                className={cn({ 'is-active': tab.id === activeTabId })}
+                key={tab.id}
+              >
+                <Link
+                  to={`/tabs/${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (onTabSelected && tab.id !== activeTabId) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {activeTab ? activeTab.content : 'Please select a tab'}
+        {currentActiveTab ? currentActiveTab.content : 'Please select a tab'}
       </div>
     </>
   );
--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Tab } from '../types/Tab';
 import { Tabs } from './Tabs';
+import { useParams } from 'react-router-dom';
 
 type Props = {
   tabs: Tab[];
 };
 
 export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams<{ tabId?: string }>();
+
   return (
     <>
       <h1 className="title">Tabs page</h1>
-      <Tabs tabs={tabs} />
+      <Tabs tabs={tabs} activeTabId={tabId} />
     </>
   );
 };
