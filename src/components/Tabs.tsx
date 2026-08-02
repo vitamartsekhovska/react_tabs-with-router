b9a388c200c640e1ab1f35e6517da1492d292eef
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-08-02 19:03:24 +0300

    add task solution finally

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import cn from 'classnames';
-import { Tab } from '../types/Tab';
+import { Tab as TabType } from '../types/Tab';
 import { Link } from 'react-router-dom';
 
 type Props = {
-  tabs: Tab[];
+  tabs: TabType[];
   activeTabId?: string;
-  onTabSelected?: (id: string) => void;
 };
 
-export const Tabs: React.FC<Props> = ({ tabs, activeTabId, onTabSelected }) => {
+export const Tabs: React.FC<Props> = ({ tabs, activeTabId }) => {
   const currentActiveTab = tabs.find(tab => tab.id === activeTabId);
 
   return (
@@ -23,15 +22,7 @@ export const Tabs: React.FC<Props> = ({ tabs, activeTabId, onTabSelected }) => {
                 className={cn({ 'is-active': tab.id === activeTabId })}
                 key={tab.id}
               >
-                <Link
-                  to={`/tabs/${tab.id}`}
-                  data-cy="TabLink"
-                  onClick={() => {
-                    if (onTabSelected && tab.id !== activeTabId) {
-                      onTabSelected(tab.id);
-                    }
-                  }}
-                >
+                <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
                   {tab.title}
                 </Link>
               </li>
